5b52d292dd31c71c23a4fac02312ccaa8de2b218
giwajossy <giwajosef@gmail.com>
2020-09-11 03:31:04 +0100

    Troubleshoot app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,12 +31,10 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+  
 var slideIndex = 1;
 showSlides(slideIndex);
-  }
-
-
-
 
 
 // Next/previous controls
