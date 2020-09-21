4662e279b830a87c30a2f9ce680a350e26c466df
Tijani-zainab <jani.tijj@gmail.com>
2020-09-21 08:49:13 +0100

    fixed the carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,5 @@
-// Darkmode
-
-const darkToggle = document.querySelector('.switch');
-const checkbox = document.querySelector('input[type="checkbox"]');
-
-darkToggle.addEventListener('click', ()=>{
-  checkbox.checked ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
-});
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
-
 
 // Next/previous controls
 function plusSlides(n) {
@@ -63,19 +26,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-function myFunction() {
-  var x = document.getElementById("navbar");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
-
-
-
-
-
-
